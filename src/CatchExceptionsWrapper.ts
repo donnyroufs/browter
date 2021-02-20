69e1114f665ff092069215dc3666ba195c7a3043
Donny Roufs <iamchets@yahoo.com>
2021-02-20 14:22:04 +0100

    Fix Catching exceptions

--- src/CatchExceptionsWrapper.ts
+++ src/CatchExceptionsWrapper.ts
@@ -1,15 +1,15 @@
 import { Request, Response, NextFunction } from "express";
 
-export function CatchExceptionsWrapper(controller: any) {
+export function CatchExceptionsWrapper(routeHandler: any) {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
-      await controller(req, res);
+      await routeHandler(req, res);
     } catch (err) {
       if (
         process.env.NODE_ENV === "dev" ||
         process.env.NODE_ENV === "development"
       ) {
-        console.info(err);
+        console.log(err);
       }
       next(err);
     }
