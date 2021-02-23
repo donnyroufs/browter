ec54554bff19b160e6da5681c1f0d38347b7076e
Donny Roufs <iamchets@yahoo.com>
2021-02-23 10:54:23 +0100

    Add middleware example

--- examples/rest-api/Routes.ts
+++ examples/rest-api/Routes.ts
@@ -1,5 +1,6 @@
 import { Browter } from '../../src/index'
 import Path from 'path'
+import { LoggerMiddleware } from './Api/Middleware/Logger.middleware'
 
 // For this example the relative path is wrong so we need to overwrite it.
 const controllersDir = Path.resolve('./examples/rest-api/Api/Controllers/index')
@@ -8,7 +9,7 @@ const browter = new Browter({
   controllersDir,
 })
 
-browter.get('/', 'CoreController.index')
+browter.get('/', 'CoreController.index', [LoggerMiddleware])
 
 browter.group('users', (browter) => {
   browter.get('/', 'UserController.index')
